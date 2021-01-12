c2453cfb26a8b04685b0c3c09b03902d144ee995
birukyemane <bir_yem@yahoo.com>
2021-01-13 00:45:11 +0200

    - Styles - Themes(Typography)

--- src/components/ui/Theme.js
+++ src/components/ui/Theme.js
@@ -3,15 +3,22 @@ import { createMuiTheme } from '@material-ui/core/styles';
 const arcBlue = "#0B72B9"
 const arcOrange = "#FFBA60"
 
-export default createMuiTheme({palette: {
-    common: {
-        arBlue: `${arcBlue}`,
-        arcOrange: `${arcOrange}`
+export default createMuiTheme({
+    palette: {
+        common: {
+            arBlue: `${arcBlue}`,
+            arcOrange: `${arcOrange}`
+        },
+        primary: {
+            main: `${arcBlue}`
+        },
+        secondary: {
+            main: `${arcOrange}`
+        }
     },
-    primary: {
-        main: `${arcBlue}`
-    },
-    secondary: {
-        main: `${arcOrange}`
+    typography: {
+        h3: {
+            fontWeight: 300
+        }
     }
-}});
+});
