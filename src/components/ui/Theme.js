5ca3e6f242f3aa08e2341c0828a7931bcb53e67b
birukyemane <bir_yem@yahoo.com>
2021-01-16 21:47:05 +0200

    - format file

--- src/components/ui/Theme.js
+++ src/components/ui/Theme.js
@@ -3,7 +3,7 @@ import { createMuiTheme } from '@material-ui/core/styles';
 const arcBlue = "#0B72B9"
 const arcOrange = "#FFBA60"
 
-export default createMuiTheme({
+export default createMuiTheme({ 
     palette: {
         common: {
             arBlue: `${arcBlue}`,
