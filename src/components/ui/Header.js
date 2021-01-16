1406d6765047d1903822aa17625f2a5646d3745c
birukyemane <bir_yem@yahoo.com>
2021-01-16 21:46:59 +0200

    -  format file

--- src/components/ui/Header.js
+++ src/components/ui/Header.js
@@ -12,7 +12,7 @@ function ElevationScroll(props) {
       threshold: 0,
     });
   
-    return React.cloneElement(children, {
+    return React.cloneElement(children, { 
       elevation: trigger ? 4 : 0,
     });
   }  
